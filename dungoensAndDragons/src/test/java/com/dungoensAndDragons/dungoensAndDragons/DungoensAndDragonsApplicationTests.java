57ed5fa502d65415c0ac0e1690da13590faa206b
Erikas J. <erikas.j1@gmail.com>
2024-10-22 13:18:42 +0300

    Project set up

---
+++ dungoensAndDragons/src/test/java/com/dungoensAndDragons/dungoensAndDragons/DungoensAndDragonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungoensAndDragons.dungoensAndDragons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungoensAndDragonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
